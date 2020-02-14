1d41845778e9e070f13acd72281feedd6a96ffbb
Kanata <kanata@MacBook-005.local>
2020-02-14 18:42:38 +0900

    initial commit

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark fiexd-top">
+                    <a 
+                    href="" 
+                    className="navbar-brand col-sm-5 col-md-2 mr-0 align-items-center"
+                    >
+                        pokedex
+                    </a>
+                </nav>
+            </div>
+        )
+    }
+}
