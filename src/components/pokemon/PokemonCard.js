6de016d90d294214153104a0dff573b4b6891e46
Kanata <kanata@MacBook-005.local>
2020-02-14 18:53:53 +0900

    add state

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
 export default class PokemonCard extends Component {
+    state = {
+        name: '',
+        imageUrl: '',
+        pokemonIndex: ''
+    }
     render() {
         return (
             <div className="col-md-3 col-sm-6 mb-5">
                 <div className="card">
                     <div className="card-header">
-                        <h1>Kanata</h1>
                     </div>
                 </div>
             </div>
