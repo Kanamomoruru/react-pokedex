75537473af4c1943cfa1c180fb2f5b9eae7d7634
Kanata <kanata@MacBook-005.local>
2020-02-17 11:45:15 +0900

    add card layout

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -6,11 +6,34 @@ export default class PokemonCard extends Component {
         imageUrl: '',
         pokemonIndex: ''
     }
+
+    componentDidMount() {
+        const {name, url} = this.props;
+        const pokemonIndex = url.split('/')[url.split('/').length - 2];
+        const imageUrl = `https://github.com/PokeApi/sprits/blob/master/sprits/pokemon/${pokemonIndex}.png?raw=true`;
+
+        this.setState({
+            name,
+            imageUrl,
+            pokemonIndex
+        });
+    }
+
     render() {
         return (
             <div className="col-md-3 col-sm-6 mb-5">
                 <div className="card">
-                    <div className="card-header">
+                    <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                    <div className="card-body mx-auto">
+                        <h6 className="card-title">
+                            {this.state.name
+                            .toLowerCase()
+                            .split(' ')
+                            .map(
+                              letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                            )
+                              .join(' ')}
+                        </h6>
                     </div>
                 </div>
             </div>
